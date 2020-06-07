769b0dc9fee62b21355bf6225f5855b185553907
Nasim Ahmed <nasimuddin1140@gmail.com>
2020-06-07 16:23:00 +0600

    Authantication

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,10 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth']], function (){
-
-    Route::get('deshboard','AdminController@deshboard');
+Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin'], function (){
+    Route::match(['get','post'],'/','AdminController@login');
+    Route::group(['middleware' =>['admin']],function(){
+            Route::get('deshboard','AdminController@deshboard');
+    });
+    
 });
